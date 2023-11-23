3e1e458c2185cf3ae40134d6bc2390b0e862a1b7
l <543923495@qq.com>
2023-11-24 04:18:01 +0800

    uri添加"/"

--- ticket/src/main/java/com/yhbl/ticket/controller/MasterController.java
+++ ticket/src/main/java/com/yhbl/ticket/controller/MasterController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({"api/v1/pub/ticket"})
+@RequestMapping({"/api/v1/pub/ticket"})
 public class MasterController {
 
     @GetMapping({"/getTicket"})
