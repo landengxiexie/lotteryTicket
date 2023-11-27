27fab4ffc05e258179d17e17680795013ae42f9c
landengxiexie <543923495@qq.com>
2023-11-27 16:31:34 +0800

    设置注数限制

--- ticket/src/main/java/com/yhbl/ticket/controller/MasterController.java
+++ ticket/src/main/java/com/yhbl/ticket/controller/MasterController.java
@@ -20,6 +20,9 @@ public class MasterController {
 
     @GetMapping({"/getTicket"})
     public Object ticket(Integer lts, Integer tbts) {
+	if(lts>1000||tbts>1000){
+		return "输入注数应≤1000";
+	}
         Map<Object, Object> multiple = Ticket.multiple(lts, tbts);
         if (multiple==null){
             return "null";
